4c6b903915b4589ce6095aec9640eaa45b465dfb
fearless-command-2358 <iuriseara@gmail.com>
2020-06-30 19:52:04 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,16 +1,5 @@
 require 'pry'
 
-# def find_item_by_name_in_collection(name, collection)
-#   counter = 0
-#   while counter < collection.length
-#     if collection[counter][:item] == name
-#       return collection[counter]
-#     end
-#     counter += 1
-#   end
-# end
-
-
 def find_item_by_name_in_collection(name, collection)
   collection.each do |element|
     element.each do |key,value|
